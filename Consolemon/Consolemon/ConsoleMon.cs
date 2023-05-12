ab5a2bcf5fdaafa71c7c073f899df89e1eafdd84
Seinaru <34807@ma-web.nl>
2023-05-12 10:00:10 +0200

    2 opdrachten

---
+++ Consolemon/Consolemon/ConsoleMon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consolemon
+{
+    internal class ConsoleMon
+    {
+        internal int health = 200;
+        internal int energy = 12;
+        internal string name = "Joe";
+
+        internal void TakeDamage(int damage)
+        {
+            this.health -= damage;
+        }
+        internal void DepleteEnergy(int energy)
+        {
+            this.energy -= energy;
+        }
+    }
+    
+}
